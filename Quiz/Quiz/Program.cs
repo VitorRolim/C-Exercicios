7038b1482426f0346201d59f00570b257fa4b2dd
Vitor Ramos <vitor_rolim11@hotmail.com.br>
2024-09-23 00:33:07 -0300

    Adicionando um jogo de quiz que utiliza o Dictionary

---
+++ Quiz/Quiz/Program.cs
@@ -0,0 +1,23 @@
+﻿Dictionary<string, string> quiz = new Dictionary<string, string>();
+
+quiz.Add("Qual a capital do Rio de janeiro?", "Rio de Janeiro");
+quiz.Add("Qual a capital do Brasil?", "Brasilia");
+quiz.Add("Quem ganhou o Goty de 2020", "The Last of Us 2");
+
+int pontuacao = 0;
+
+foreach(var pergunta in quiz)
+{
+    Console.WriteLine(pergunta.Key);
+    string resposta = Console.ReadLine();
+    if(resposta == pergunta.Value)
+    {
+        Console.WriteLine("Parabéns a resposta esta certa");
+        pontuacao++;
+        Console.WriteLine("Sua pontuação é de: "+ pontuacao);
+    }
+    else
+    {
+        Console.WriteLine("Que pena voce errou");
+    }
+}
