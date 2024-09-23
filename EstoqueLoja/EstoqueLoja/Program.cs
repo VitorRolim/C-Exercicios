98da185f48557cb6c608f07022dd40296ef1fcc9
Vitor Ramos <vitor_rolim11@hotmail.com.br>
2024-09-23 00:38:19 -0300

    Exercicio sobre adicionar item no Dictionary

---
+++ EstoqueLoja/EstoqueLoja/Program.cs
@@ -0,0 +1,15 @@
+﻿Dictionary<string, int> estoqueLoja = new Dictionary<string, int>();
+
+Console.Write("Digite o nome do produto: ");
+string nomeProduto = Console.ReadLine();
+Console.Write("Digite a quantidade de produtos: ");
+int qtdeProduto = int.Parse(Console.ReadLine());
+
+
+estoqueLoja.Add(nomeProduto,qtdeProduto);
+
+string produto = nomeProduto;
+
+Console.WriteLine($"Produto: {produto}: {estoqueLoja[produto]}");
+
+
